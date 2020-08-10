26501581164a675c87a158dacc6614197034adb9
Igor <eaglemihawk@gmail.com>
2020-08-10 23:35:38 +0900

    jsx lesson

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.css'; 
+
+const userName = 'Igor';
+const age = 30;
+const human = true;
 
 function App() {
   return (
@@ -10,8 +14,28 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <h2><i>Hello Hello Hello</i>
+        <h2 style={{
+          fontSize: 20 + 12
+        }}>
+        <i>Hello Hello Hello</i>
         </h2>
+        <p>
+        my name: {userName}
+        </p>
+        <p>
+        my age: {age}
+        </p>
+        <p>
+        {human && 'I am from Earth'}
+        </p>
+        <p>
+        {human ? 'i am born on Earth' : 'i am not born on Earth'}
+        {null}
+        {undefined}
+        {false}
+        {true}
+        {human}
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
