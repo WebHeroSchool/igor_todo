5451b15142dfef7df7fd054533c3b2c3f392892f
Igor <eaglemihawk@gmail.com>
2020-08-12 20:23:54 +0900

    jsx

---
+++ build/precache-manifest.5e331d8cce17c34afd83e54be500cd2d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd79300ceadade4c643ce35f5552a328",
+    "url": "/index.html"
+  },
+  {
+    "revision": "93049324a45e9eacd974",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "895d31552c51ba2eccbe",
+    "url": "/static/js/2.53d0a898.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.53d0a898.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93049324a45e9eacd974",
+    "url": "/static/js/main.8b5e0556.chunk.js"
+  },
+  {
+    "revision": "559ff28579f192bdf8b9",
+    "url": "/static/js/runtime-main.98bec8e4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
