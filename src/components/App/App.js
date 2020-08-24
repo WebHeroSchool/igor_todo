abb04e64f874c5f9104a6a58d1db63cbe40baab3
Igor <eaglemihawk@gmail.com>
2020-08-24 22:48:15 +0900

    add folder with components

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import InputItem from '../InputItem/InputItem';
+import Footer from '../Footer/Footer';
+
+const App = () => (
+	<div>
+	<h2>Важные дела:</h2>
+		<InputItem />
+		<Footer />
+	</div>
+);
+
+export default App;
---
+++ src/components/InputItem/InputItem.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ItemList from '../ItemList/ItemList';
+
+const InputItem = () => (
+	<div>InputItem
+		<ItemList />
+	</div>
+);
+
+export default InputItem;
