90515acaf38c00078ed238c287c398da78461d88
Igor <eaglemihawk@gmail.com>
2020-08-30 20:36:25 +0900

    add .map and key

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,14 +3,28 @@ import InputItem from '../InputItem/InputItem';
 import ItemList from '../ItemList/ItemList';
 import Footer from '../Footer/Footer';
 
-const todo = ['Купить хлеб', 'Купить молоко', 'Купить масло'];
-const App = () => (
+
+const App = () => {
+	
+	const items = [
+		{
+			value: 'Купить хлеб'
+		},
+		{
+			value: 'Купить молоко'
+		},
+		{
+			value: 'Купить масло'
+		}
+	];
+
+	return (
 	<div>
 	<h2>Важные дела:</h2>
 		<InputItem />
-		<ItemList todoItems={todo}/>
+		<ItemList items={items}/>
 		<Footer count={5}/>
-	</div>
-);
+	</div>);
+}
 
 export default App;
