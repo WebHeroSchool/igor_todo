72b325e9f967f0cd48feb59e01beedfe674428cd
Igor <eaglemihawk@gmail.com>
2020-09-06 14:05:43 +0900

    install ui design

--- src/components/InputItem/InputItem.js
+++ src/components/InputItem/InputItem.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import styles from './InputItem.module.css';
+import TextField from '@material-ui/core/TextField';
 
 const InputItem = () => (
-	<div className={styles.input}>Введите новое дело
+	<div className={styles.input}>
+		<TextField 
+		id="outlined-basic" 
+		label="Новое задание" 
+		variant="outlined" />
 	</div>
 );
 
